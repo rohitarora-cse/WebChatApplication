da1404d172cc72a04a4319cb09237626adb7737e
rarora <rarora@qasource.com>
2014-10-03 19:30:17 +0530

    added socket.io api and gave basic chat functionality

--- application.js
+++ application.js
@@ -1,9 +1,24 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var socketManager = require('socket.io')(http);
+var options = {root: __dirname};
 
 app.get('/',function(req,res)
 {
-	res.send('Hello World\n');
+	res.sendFile('index.html',options);
 });
+
+socketManager.on('connection',function(socket)
+		{
+			console.log('user connected');
+			socket.on('message',function(msg){
+				socketManager.emit('message', msg);
+			});
+			socket.on('disconnect',function(socket)
+					{
+				console.log('user disconnected');
+			});
+		});
+
 http.listen(8080);
 console.log('server running');
